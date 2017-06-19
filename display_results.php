46075bbfca9c967c9fa6b184a96095b359c4f493
Tagrat <tagg12352@gmail.com>
2017-06-19 11:02:27 -0400

    Fixed display php

--- display_results.php
+++ display_results.php
@@ -17,6 +17,8 @@
         $error_message = 'Interest rate must be a valid number.'; 
     } else if ( $interest_rate <= 0 ) {
         $error_message = 'Interest rate must be greater than zero.'; 
+    } else if ( $interest_rate > 15 ) {
+        $error_message = 'Interest rate must be less than or equal to 15.';
     // validate years
     } else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
@@ -67,6 +69,8 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+
+        <p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
     </main>
 </body>
 </html>
